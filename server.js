54d1e6b9a3dc85b97ae01f720815c11522d00782
Ayush <ayush.krsh9@gmail.com>
2023-01-30 15:00:47 +0530

    Endpoint created

--- server.js
+++ server.js
@@ -2,9 +2,13 @@ const express = require('express')
 
 const app = express();
 
-const port = 3000;
+
 
 const dbconfig = require('./db');
 
+const roomsRoute = require('./routes/roomsRoute');
 
+app.use('/api/rooms', roomsRoute);
+
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
